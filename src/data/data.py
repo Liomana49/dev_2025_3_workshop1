f424b89a6658d0a20902229c5010c96efcbcb6a8
liomana49 <liomana49@ucatolica.edu.co>
2025-08-20 10:33:37 -0600

    taller

--- src/data/data.py
+++ src/data/data.py
@@ -26,17 +26,18 @@ class Data:
         return resultado
     
     def merge_ordenado(self, lista1, lista2):
-        """
-        Combina dos listas ordenadas en una sola lista ordenada.
-        
-        Args:
-            lista1 (list): Primera lista ordenada
-            lista2 (list): Segunda lista ordenada
-            
-        Returns:
-            list: Lista combinada y ordenada
-        """
-        pass
+        resultado = []
+        i, j = 0, 0
+        while i < len(lista1) and j < len(lista2):
+            if lista1[i] < lista2[j]:
+                resultado.append(lista1[i])
+                i += 1
+            else:
+                resultado.append(lista2[j])
+                j += 1
+        resultado.extend(lista1[i:])
+        resultado.extend(lista2[j:])
+        return resultado
     
     def rotar_lista(self, lista, k):
         """
