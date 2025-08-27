1afd744797c65924c7347da50f50bd9502992fe6
liomana49 <liomana49@ucatolica.edu.co>
2025-08-27 10:46:33 -0600

    fibonacci 4

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -24,7 +24,8 @@ class Magic:
         for i in range(2, int(n ** 0.5) + 1):
             if n % i == 0:
                 return False
-        return Truegi
+        return True
+    
     def generar_primos(self, n):
         """
         Genera una lista de números primos hasta n.
